43f31b450d51cc9b45b6ecfde3b3065a2b7ea20c
VEEREPALLI SHANMUKH <shanmukhchowdary63@gmail.com>
2026-01-08 13:31:01 +0530

    fix: Update upload domain to movielovers.in and enable PHP debugging

--- public/upload-profile.php
+++ public/upload-profile.php
@@ -3,6 +3,11 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST');
 header('Content-Type: application/json');
 
+// ENABLE DEBUGGING
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
     echo json_encode(['error' => 'Method not allowed']);
